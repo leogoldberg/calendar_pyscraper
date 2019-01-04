2e9454fb07e131f977fbaacfdc47f08074521b36
lgoldber <lgoldber@sfu.ca>
2019-01-03 20:31:58 -0800

    initial commit

--- pyscraper.py
+++ pyscraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from csv import writer
 from datetime import *
 
-response = requests.get('http://www.sfu.ca/outlines.html?2019/spring/cmpt/276/d100')
+response = requests.get('http://www.sfu.ca/outlines.html?2019/spring/cmpt/295/d100')
 
 soup = BeautifulSoup(response.text, 'html.parser')
 # get_text()
@@ -14,7 +14,7 @@ name = soup.find(attrs={"id": "name"}).get_text()
 room_times = soup.find(class_='course-times')
 room_check = room_times.contents
 
-print(room_check)
+# print(room_check)
 
 
 def room_parser(room, time_day):
@@ -22,7 +22,11 @@ def room_parser(room, time_day):
     room_info = re.search('(.*)\s\d+', room)[0]
     time_start = re.search('\d+\:\d+\s[a-zA-Z]+', time_day)[0]
     time_end = re.search('(?<=– )(.*)', time_day)[0]  # match everything after pesky en dash!
-    return [room_info, time_start, time_end]
+
+    # matches if space followed by number follows string
+    day = re.search('(.*)(?=\s\d+:\d+\s[A-Z]{2}\s–)', time_day)[0]
+
+    return [room_info, time_start, time_end, day]
 
 
 def name_parser(name):
@@ -35,6 +39,9 @@ if(len(room_check) < 7):
     room = room_times.contents[2]
 
     name = name_parser(name)
+    info = room_parser(room, time_day)
+    print(name)
+    print(info)
 
 else:
     room_times1 = room_times.contents[2].find_next_sibling()
@@ -48,24 +55,6 @@ else:
     name = name_parser(name)
     info1 = room_parser(room1, time_day1)
     info2 = room_parser(room2, time_day2)
-
-    print(time_day1)
-    print(time_day2)
-    print(room1)
-    print(room2)
-
-print(name)
-
-room = re.search('(.*)\s\d+', room)[0]  # anything followed by space followed by one or more digits
-name = re.search('(?<=-\s)(.*)$', name)[0]  # match everything after '- '
-time_start = re.search('\d+\:\d+\s[a-zA-Z]+', time_day)[0]
-time_end = re.search('(?<=– )(.*)', time_day)[0]  # match everything after pesky en dash!
-
-# matches if space followed by number follows string
-day = re.search('(.*)(?=\s\d+:\d+\s[A-Z]{2}\s–)', time_day)[0]
-
-print(name)
-print(room)
-print(time_start)
-print(time_end)
-print(day)
+    print(name)
+    print(info1)
+    print(info2)
