af07ab31ad56ee8f875af6a95dd3c4c71df4ff2c
lgoldber <lgoldber@sfu.ca>
2019-01-03 20:53:45 -0800

    deleted course class, added date_split to pyscraper

--- pyscraper.py
+++ pyscraper.py
@@ -17,6 +17,22 @@ room_check = room_times.contents
 # print(room_check)
 
 
+def date_split(date):
+    date_list = []
+    date_len = len(date)
+    if date_len == 6:
+        date_list = [date[0:2], date[4:6]]
+        return date_list
+    elif date_len == 10:
+        date_list = [date[0:2], date[4:6], date[8:10]]
+        return date_list
+    elif date_len == 2:
+        date_list = [date]
+        return date_list
+    else:
+        print("Ill formated date string")
+
+
 def room_parser(room, time_day):
     # anything followed by space followed by one or more digits
     room_info = re.search('(.*)\s\d+', room)[0]
@@ -33,6 +49,7 @@ def name_parser(name):
     return re.search('(?<=-\s)(.*)$', name)[0]  # match everything after '- '
 
 
+# main processing:
 if(len(room_check) < 7):
     room_times = room_times.contents[2].find_next_sibling()
     time_day = room_times.contents[0]
@@ -40,8 +57,10 @@ if(len(room_check) < 7):
 
     name = name_parser(name)
     info = room_parser(room, time_day)
+    date_list = date_split(info[3])
     print(name)
     print(info)
+    print(date_list)
 
 else:
     room_times1 = room_times.contents[2].find_next_sibling()
@@ -55,6 +74,10 @@ else:
     name = name_parser(name)
     info1 = room_parser(room1, time_day1)
     info2 = room_parser(room2, time_day2)
+    date_list1 = date_split(info1[3])
+    date_list2 = date_split(info2[3])
     print(name)
     print(info1)
     print(info2)
+    print(date_list1)
+    print(date_list2)
